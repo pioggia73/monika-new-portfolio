7d189e0db9653571b4dbc477c00dc10ced9c13cc
Monika Wasilewska <monikawasilewska13@wp.pl>
2020-10-11 18:36:54 +0200

    featured projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,46 +1,46 @@
-// import React from 'react';
-// import {graphql} from 'gatsby';
-// /*     components     */
-// import Layout from '../components/Layout';
-// import Projects from '../components/Projects';
+import React from 'react';
+import {graphql} from 'gatsby';
+/*     components     */
+import Layout from '../components/Layout';
+import Projects from '../components/Projects';
 
-// const ProjectsPage = ({data}) => {
+const ProjectsPage = ({data}) => {
 
-//   const {allAirtable: {nodes: projects}} = data
+  const {allAirtable: {nodes: projects},} = data
 
-//     return (
-//             <Layout>
-//                 <section className='projects-page'>
-//                     <Projects projects={projects} title='all projects' /> 
-//                 </section>
-//             </Layout>
-//     )
-// };
+    return (
+            <Layout>
+                <section className='projects-page'>
+                    <Projects projects={projects} title='all projects' /> 
+                </section>
+            </Layout>
+    )
+};
 
-// export const query = graphql`
-//   {
-//     allAirtable(filter:{ table: {eq: "projects"}}) {
-//       nodes {
-//         data {
-//           description
-//           github_url
-//           name
-//           pageurl
-//           stack
-//           image {
-//             localFiles {
-//               childImageSharp {
-//                 fluid {
-//                   ...GatsbyImageSharpFluid
-//                 }
-//                 id
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  {
+    allAirtable(filter:{ table: {eq: "projects"}}) {
+      nodes {
+        data {
+          description
+          github_url
+          name
+          pageurl
+          stack
+          image {
+            localFiles {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
-// export default ProjectsPage;
+export default ProjectsPage;
